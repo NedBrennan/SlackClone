ced9ad7da078c6d9317429c0d376b6d070b27a71
Ned Brennan <AppleSeed@NedsMac.local>
2021-03-24 20:47:04 -0400

    firebase

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCQI-zPtIqjEA1CZln57QVMnfnYzfSuaMU",
+  authDomain: "slack-3aa42.firebaseapp.com",
+  projectId: "slack-3aa42",
+  storageBucket: "slack-3aa42.appspot.com",
+  messagingSenderId: "864875561375",
+  appId: "1:864875561375:web:85407c11c292714dcd7daa"
+};
